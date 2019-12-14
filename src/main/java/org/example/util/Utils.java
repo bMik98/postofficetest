52d9763c0b7cdc4ad220bc460d29221cc4c4e26f
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-12-14 03:43:44 +0200

    utils

--- src/main/java/org/example/util/Utils.java
+++ src/main/java/org/example/util/Utils.java
@@ -5,36 +5,36 @@ import org.example.model.Package;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class Utils {
 
-    public static final Function<Box, Integer> calculateBoxInternalHeight = box ->
+    public static final ToIntFunction<Box> calculateBoxInternalHeight = box ->
             box.getHeight() - (box.getWallThickness() << 1);
 
-    public static final Function<Box, Integer> calculateBoxInternalWidth = box ->
+    public static final ToIntFunction<Box> calculateBoxInternalWidth = box ->
             box.getWidth() - (box.getWallThickness() << 1);
 
-    public static final Function<Box, Integer> calculateBoxInternalLength = box ->
+    public static final ToIntFunction<Box> calculateBoxInternalLength = box ->
             box.getLength() - (box.getWallThickness() << 1);
 
-    public static final Function<Box, Integer> calculateBoxInternalVolume = box ->
-            calculateBoxInternalLength.apply(box)
-                    * calculateBoxInternalWidth.apply(box)
-                    * calculateBoxInternalHeight.apply(box);
+    public static final ToIntFunction<Box> calculateBoxInternalVolume = box ->
+            calculateBoxInternalLength.applyAsInt(box)
+                    * calculateBoxInternalWidth.applyAsInt(box)
+                    * calculateBoxInternalHeight.applyAsInt(box);
 
     private Utils() {
     }
 
     public static boolean isPackageFitInBox(Package parcel, Box box) {
-        return parcel.getHeight() <= calculateBoxInternalHeight.apply(box)
-                && parcel.getWidth() <= calculateBoxInternalWidth.apply(box)
-                && parcel.getLength() <= calculateBoxInternalLength.apply(box);
+        return parcel.getHeight() <= calculateBoxInternalHeight.applyAsInt(box)
+                && parcel.getWidth() <= calculateBoxInternalWidth.applyAsInt(box)
+                && parcel.getLength() <= calculateBoxInternalLength.applyAsInt(box);
     }
 
     public static Optional<Box> selectLeastSuitableBox(Package parcel, List<Box> boxes) {
         return boxes.stream()
                 .filter(box -> isPackageFitInBox(parcel, box))
-                .min((o1, o2) -> calculateBoxInternalVolume.apply(o2) - calculateBoxInternalVolume.apply(o1));
+                .min((o1, o2) -> calculateBoxInternalVolume.applyAsInt(o2) - calculateBoxInternalVolume.applyAsInt(o1));
     }
 }
