8f65205f8e615e62129ba339dff524d666cb554a
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-12-14 03:34:15 +0200

    utils

--- src/main/java/org/example/util/Utils.java
+++ src/main/java/org/example/util/Utils.java
@@ -4,16 +4,24 @@ import org.example.model.Box;
 import org.example.model.Package;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class Utils {
 
     public static final Function<Box, Integer> calculateBoxInternalHeight = box ->
             box.getHeight() - (box.getWallThickness() << 1);
+
     public static final Function<Box, Integer> calculateBoxInternalWidth = box ->
             box.getWidth() - (box.getWallThickness() << 1);
+
     public static final Function<Box, Integer> calculateBoxInternalLength = box ->
             box.getLength() - (box.getWallThickness() << 1);
+
+    public static final Function<Box, Integer> calculateBoxInternalVolume = box ->
+            calculateBoxInternalLength.apply(box)
+                    * calculateBoxInternalWidth.apply(box)
+                    * calculateBoxInternalHeight.apply(box);
 
     private Utils() {
     }
@@ -24,7 +32,9 @@ public class Utils {
                 && parcel.getLength() <= calculateBoxInternalLength.apply(box);
     }
 
-    public static Box selectLeastSuitableBox(Package parcel, List<Box> boxes) {
-        return
+    public static Optional<Box> selectLeastSuitableBox(Package parcel, List<Box> boxes) {
+        return boxes.stream()
+                .filter(box -> isPackageFitInBox(parcel, box))
+                .min((o1, o2) -> calculateBoxInternalVolume.apply(o2) - calculateBoxInternalVolume.apply(o1));
     }
 }
