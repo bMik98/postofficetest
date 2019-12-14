9abde647922e88205ba949510687cc17901bf4e9
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-12-14 03:01:46 +0200

    builders

--- src/main/java/org/example/builder/BoxBuilder.java
+++ src/main/java/org/example/builder/BoxBuilder.java
@@ -1,16 +1,65 @@
-package org.example.factory;
+package org.example.builder;
 
+import org.example.exception.IllegalHeightValueException;
+import org.example.exception.IllegalLengthValueException;
+import org.example.exception.IllegalWallThicknessValueException;
+import org.example.exception.IllegalWidthValueException;
 import org.example.model.Box;
 
-public class BoxFactory {
+public class BoxBuilder {
 
-    public final static int MIN_SPACE_IN_MILLIMETRES = 10;
-    public final static int MIN_WALL_THICKNESS_IN_MILLIMETRES = 10;
+    public static final int MIN_SPACE_IN_MILLIMETRES = 10;
+    public static final int MIN_WALL_THICKNESS_IN_MILLIMETRES = 1;
 
-    private BoxFactory() {
+    private int length;
+    private int width;
+    private int height;
+    private int wallThickness;
+
+    public BoxBuilder() {
+        this.length = MIN_SPACE_IN_MILLIMETRES;
+        this.width = MIN_SPACE_IN_MILLIMETRES;
+        this.height = MIN_SPACE_IN_MILLIMETRES;
+        this.wallThickness = MIN_WALL_THICKNESS_IN_MILLIMETRES;
     }
 
-    public static Box createInMillimetres(length) {
-        return new Box()
+    public BoxBuilder length(Dimension dimension) {
+        int value = dimension.getValueInMillimetres();
+        if (value < MIN_SPACE_IN_MILLIMETRES) {
+            throw new IllegalLengthValueException(value, MIN_SPACE_IN_MILLIMETRES);
+        }
+        this.length = value;
+        return this;
+    }
+
+    public BoxBuilder width(Dimension dimension) {
+        int value = dimension.getValueInMillimetres();
+        if (value < MIN_SPACE_IN_MILLIMETRES) {
+            throw new IllegalWidthValueException(value, MIN_SPACE_IN_MILLIMETRES);
+        }
+        this.width = value;
+        return this;
+    }
+
+    public BoxBuilder height(Dimension dimension) {
+        int value = dimension.getValueInMillimetres();
+        if (value < MIN_SPACE_IN_MILLIMETRES) {
+            throw new IllegalHeightValueException(value, MIN_SPACE_IN_MILLIMETRES);
+        }
+        this.height = value;
+        return this;
+    }
+
+    public BoxBuilder wallThickness(Dimension dimension) {
+        int value = dimension.getValueInMillimetres();
+        if (value < MIN_WALL_THICKNESS_IN_MILLIMETRES) {
+            throw new IllegalWallThicknessValueException(value, MIN_WALL_THICKNESS_IN_MILLIMETRES);
+        }
+        this.wallThickness = value;
+        return this;
+    }
+
+    public Box build() {
+        return new Box(length, width, height, wallThickness);
     }
 }
